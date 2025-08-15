d59cb2889b0501d278a53814bda7ebeb2b35de0e
JJayexd <kontakt.emilbj@gmail.com>
2025-08-15 13:10:40 +0200

    Login tilføjet

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import { Header } from "./Components/Header/Header";
+import { Main } from "./Components/Main/Main";
+import { AppRouter } from "./Components/AppRouter/AppRouter";
 
 function App() {
   return (
     <>
       <Header />
+        <Main>
+          <AppRouter />
+        </Main>
     </>
   )
 }
